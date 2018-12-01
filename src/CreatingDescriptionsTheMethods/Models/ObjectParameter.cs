c0174f9e91eb225212e61397b45e9e31536eec4a
djserega <galyuk.sergey@gmail.com>
2018-12-01 11:22:17 +0200

    added: class TypeObjectParameter

--- src/CreatingDescriptionsTheMethods/Models/ObjectParameter.cs
+++ src/CreatingDescriptionsTheMethods/Models/ObjectParameter.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CreatingDescriptionsTheMethods.Models
 {
-    internal class ObjectParameter
+    internal class ObjectParameter : TypeObjectParameter
     {
         public ObjectParameter()
         {
@@ -30,40 +30,8 @@ namespace CreatingDescriptionsTheMethods.Models
             Description = description;
         }
 
-        internal string Name { get; set; }
-        internal string Type { get; set; }
+        internal override string Name { get; set; }
+        internal override string Type { get; set; }
         internal string Description { get; set; }
-
-        internal bool IsArray { get => Name.StartsWith("мс"); }
-        internal bool IsMap { get => Name.StartsWith("со"); }
-        internal bool IsStructure { get => Name.StartsWith("ст"); }
-        internal bool IsValueList { get => Name.StartsWith("сз"); }
-        internal bool IsValueTable { get => Name.StartsWith("тз"); }
-        internal bool IsValueTree { get => Name.StartsWith("дз"); }
-        internal bool IsFixedArray { get => Name.StartsWith("фмс"); }
-        internal bool IsFixedMap { get => Name.StartsWith("фсо"); }
-        internal bool IsFixedStructure { get => Name.StartsWith("фст"); }
-
-        internal void SetTypeByName()
-        {
-            if (IsArray)
-                Type = "Массив";
-            else if (IsMap)
-                Type = "Соответствие";
-            else if (IsStructure)
-                Type = "Структура";
-            else if (IsValueList)
-                Type = "СписокЗначений";
-            else if (IsValueTable)
-                Type = "ТаблицаЗначений";
-            else if (IsValueTree)
-                Type = "ДеревоЗначений";
-            else if (IsFixedArray)
-                Type = "ФиксированныйМассив";
-            else if (IsFixedMap)
-                Type = "ФиксированноеСоответствие";
-            else if (IsFixedStructure)
-                Type = "ФиксированнаяСтруктура";
-        }
     }
 }
---
+++ src/CreatingDescriptionsTheMethods/Models/TypeObjectParameter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreatingDescriptionsTheMethods.Models
+{
+    internal class TypeObjectParameter
+    {
+        internal virtual string Name { get; set; }
+        internal virtual string Type { get; set; }
+
+        internal bool IsArray { get => NameStartsWith("мс"); }
+        internal bool IsMap { get => NameStartsWith("со"); }
+        internal bool IsStructure { get => NameStartsWith("ст"); }
+        internal bool IsValueList { get => NameStartsWith("сз"); }
+        internal bool IsValueTable { get => NameStartsWith("тз"); }
+        internal bool IsValueTree { get => NameStartsWith("дз"); }
+        internal bool IsFixedArray { get => NameStartsWith("фмс"); }
+        internal bool IsFixedMap { get => NameStartsWith("фсо"); }
+        internal bool IsFixedStructure { get => NameStartsWith("фст"); }
+
+
+        private bool NameStartsWith(string text) => Name.StartsWith(text);
+
+        internal void SetTypeByName()
+        {
+            if (IsArray)
+                Type = "Массив";
+            else if (IsMap)
+                Type = "Соответствие";
+            else if (IsStructure)
+                Type = "Структура";
+            else if (IsValueList)
+                Type = "СписокЗначений";
+            else if (IsValueTable)
+                Type = "ТаблицаЗначений";
+            else if (IsValueTree)
+                Type = "ДеревоЗначений";
+            else if (IsFixedArray)
+                Type = "ФиксированныйМассив";
+            else if (IsFixedMap)
+                Type = "ФиксированноеСоответствие";
+            else if (IsFixedStructure)
+                Type = "ФиксированнаяСтруктура";
+        }
+
+    }
+}
